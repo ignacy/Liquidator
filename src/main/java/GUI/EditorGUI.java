4e618cbe45a2965aa19efa304d5f7bd6e69dcdf9
Ignacy Moryc <imoryc@gmail.com>
2019-02-15 21:28:42 +0100

    Better style

--- src/main/java/GUI/EditorGUI.java
+++ src/main/java/GUI/EditorGUI.java
@@ -1,18 +1,29 @@
 package GUI;
 
+import Liquid.Renderer;
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
 
 public class EditorGUI extends JFrame {
     public EditorGUI() {
 
-        JTextArea inputText = new JTextArea("This is some {{ name }} template ");
+        JTextArea inputText = new JTextArea("Hello {{ name }} ! ");
         inputText.setEditable(true);
+        inputText.setFont(new Font("monospaced", Font.PLAIN, 16));
+        //inputText.setFont(inputText.getFont().deriveFont(15f));
         JScrollPane scroll = new JScrollPane(inputText);
 
-        Color color = new Color(111, 134, 255);
-        JTextArea outputText = new JTextArea("This is some {{ name }} template ");
+
+        HashMap context = new HashMap<String, String>();
+        context.put("name", "WORLD");
+        Renderer view = new Renderer(inputText.getText(), context);
+
+
+        Color color = new Color(241, 255, 244);
+        JTextArea outputText = new JTextArea(view.render());
         outputText.setEditable(false);
+        outputText.setFont(inputText.getFont());
         outputText.setBackground(color);
         JScrollPane scrollOut = new JScrollPane(outputText);
 
